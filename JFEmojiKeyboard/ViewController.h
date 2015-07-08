d878a922c701087af515a194e5d9e9dfdde7536c
apple <apple>
2015-07-08 14:53:03 +0800

    Initial Commit

---
+++ JFEmojiKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFEmojiKeyboard
+//
+//  Created by junFung on 15/7/8.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JFEmojiKeyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JFEmojiKeyboard
+//
+//  Created by junFung on 15/7/8.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
